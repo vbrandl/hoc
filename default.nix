210a1f4592a8377397fc45df917b48ea245092f7
Valentin Brandl <vbrandl@riseup.net>
2020-02-15 20:26:30 +0100

    Commit nix stuff

--- default.nix
+++ default.nix
@@ -1,26 +1,25 @@
-# { system ? builtins.currentSystem }:
 { sources ? import ./nix/sources.nix
 , pkgs ? import sources.nixpkgs { }
 , callPackage ? pkgs.callPackage
 }:
 
 let
-  cargoNix = callPackage ./Cargo.nix { };
+  cargoNix = callPackage ./Cargo.nix {
+    # defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+    #   libgit2-sys = attrs: {
+    #     buildInputs = [ pkgs.openssl ];
+    #   };
+    # };
+  };
 
   hoc = cargoNix.rootCrate.build;
-
-  buildInputs = [ pkgs.openssl pkgs.cacert ];
-
-  # version = hoc.version;
-
-# in hoc.rootCrate.build
 in
 pkgs.symlinkJoin {
   name = hoc.name;
   version = hoc.crateVersion;
   paths = [ hoc ];
 
-  buildInputs = [ pkgs.openssl pkgs.cacert ];
+  # buildInputs = [ pkgs.openssl pkgs.cacert ];
 
   postBuild = ''
     rm -rf $out/bin/hoc.d
