bcb5650cc919a7d1a1c457b9dd0cfe0cf248055d
Valentin Brandl <mail+github@vbrandl.net>
2021-09-03 13:13:03 +0200

    Update actix-web and tokio

--- tests/badge.rs
+++ tests/badge.rs
@@ -1,12 +1,10 @@
 mod util;
 
-use actix_web::client;
-
 #[actix_rt::test]
 async fn badge_succeeds() {
     let test_app = util::spawn_app().await;
 
-    let client = client::Client::default();
+    let client = awc::Client::default();
 
     let response = client
         .get(&format!("{}/github/vbrandl/hoc", test_app.address))
--- tests/json.rs
+++ tests/json.rs
@@ -1,12 +1,10 @@
 mod util;
 
-use actix_web::client;
-
 #[actix_rt::test]
 async fn json_returns_success() {
     let test_app = util::spawn_app().await;
 
-    let client = client::Client::default();
+    let client = awc::Client::default();
 
     let response = client
         .get(&format!("{}/github/vbrandl/hoc/json", test_app.address))
