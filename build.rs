5f29a3edfaa5695d751f970a5d69e59e94f1e0d3
Valentin Brandl <mail+github@vbrandl.net>
2022-02-17 12:01:50 +0100

    Don't unwrap results in build step

--- build.rs
+++ build.rs
@@ -1,15 +1,12 @@
 extern crate ructe;
 extern crate vergen;
 
-use ructe::Ructe;
+use ructe::{Ructe, RucteError};
 use vergen::{vergen, Config, ShaKind};
 
-fn main() {
+fn main() -> Result<(), RucteError> {
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     vergen(config).expect("Unable to generate static repo info");
-    Ructe::from_env()
-        .expect("ructe")
-        .compile_templates("templates")
-        .unwrap();
+    Ructe::from_env()?.compile_templates("templates")
 }
