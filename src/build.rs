45b45eebc274174c6ff6de6acc7e49f2cf162862
Valentin Brandl <valentin.brandl@ebsnet.de>
2023-04-13 11:56:41 +0200

    Apply cargo fmt

--- src/build.rs
+++ src/build.rs
@@ -3,9 +3,7 @@ use ructe::Ructe;
 use vergen::EmitBuilder;
 
 fn main() -> Result<()> {
-    EmitBuilder::builder()
-        .git_sha(true)
-        .emit()?;
+    EmitBuilder::builder().git_sha(true).emit()?;
 
     let mut ructe = Ructe::from_env()?;
     let mut statics = ructe.statics()?;
